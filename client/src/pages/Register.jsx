43bccd5bb3da89e8a9d7977ee7ff08de71b52c86
Jabame <justinetaglucop@gmail.com>
2022-07-24 19:01:29 +0800

    setting up register and login data

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const { name, email, password, confirmPassword } = registerData;
+
+  const handleChange = (e) => {
+    setRegisterData((prevState) => ({
+      ...prevState,
+      [e.target.value]: e.target.name,
+    }));
+  };
+
+  const handleRegister = () => {};
+
   return (
     <div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
       <div className='max-w-lg mx-auto'>
-        <form
-          action=''
-          className='p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl'
-        >
+        <form className='p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl'>
           <h1 className='text-xl font-medium mt-5 mb-10 flex justify-center'>
             Register
           </h1>
@@ -20,8 +35,11 @@ const Register = () => {
               <input
                 type='text'
                 id='name'
+                name='name'
                 className='w-full p-3 pr-12 text-sm rounded-lg shadow-sm'
                 placeholder='Enter name'
+                value={name}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -33,8 +51,11 @@ const Register = () => {
               <input
                 type='email'
                 id='email'
+                name='email'
                 className='w-full p-3 pr-12 text-sm rounded-lg shadow-sm'
                 placeholder='Enter email'
+                value={email}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -46,8 +67,11 @@ const Register = () => {
               <input
                 type='password'
                 id='password'
+                name='password'
                 className='w-full p-3 pr-12 text-sm rounded-lg shadow-sm'
                 placeholder='Enter password'
+                value={password}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -58,15 +82,19 @@ const Register = () => {
             <div className='relative mt-1'>
               <input
                 type='password'
-                id='password'
+                id='confirmPassword'
+                name='confirmPassword'
                 className='w-full p-3 pr-12 text-sm rounded-lg shadow-sm'
                 placeholder='confirm password'
+                value={confirmPassword}
+                onChange={handleChange}
               />
             </div>
           </div>
           <button
             type='submit'
             className='block w-full px-5 py-3 text-sm font-medium text-white bg-blue-400 rounded-lg mt-5 '
+            onClick={handleRegister}
           >
             Register
           </button>
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,46 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Signin = () => {
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = loginData;
+
+  const handleChange = (e) => {
+    setLoginData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
       <div className='max-w-lg mx-auto'>
-        <form
-          action=''
-          className='p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl'
-        >
+        <form className='p-8 mt-6 mb-0 space-y-4 rounded-lg shadow-2xl'>
           <h1 className='text-xl font-medium mt-5 mb-10 flex justify-center'>
             Welcome
           </h1>
           <div>
-            <label for='email' className='text-sm font-medium'>
-              Email
-            </label>
+            <label className='text-sm font-medium'>Email</label>
             <div className='relative mt-1'>
               <input
                 type='email'
                 id='email'
+                name='email'
                 className='w-full p-3 pr-12 text-sm rounded-lg shadow-sm'
                 placeholder='Enter email'
+                value={email}
+                onChange={handleChange}
               />
             </div>
           </div>
           <div>
-            <label for='password' className='text-sm font-medium'>
-              Password
-            </label>
+            <label className='text-sm font-medium'>Password</label>
             <div className='relative mt-1'>
               <input
                 type='password'
                 id='password'
+                name='password'
                 className='w-full p-3 pr-12 text-sm rounded-lg shadow-sm'
                 placeholder='Enter password'
+                value={password}
+                onChange={handleChange}
               />
             </div>
           </div>
           <button
             type='submit'
             className='block w-full px-5 py-3 text-sm font-medium text-white bg-blue-400 rounded-lg mt-5 '
+            onClick={handleLogin}
           >
             Sign in
           </button>
